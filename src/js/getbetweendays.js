990eb9bc43352e829ed8446a91937552e473eb7a
cag2050 <cag2050@163.com>
2017-12-01 14:54:11 +0800

    m

--- src/js/getbetweendays.js
+++ src/js/getbetweendays.js
@@ -1,12 +1,3 @@
-Date.prototype.format = function () {
-    var s = '';
-    var mouth = (this.getMonth() + 1) >= 10 ? (this.getMonth() + 1) : ('0' + (this.getMonth() + 1));
-    var day = this.getDate() >= 10 ? this.getDate() : ('0' + this.getDate());
-    s += this.getFullYear() + '-'; // 获取年份。
-    s += mouth + "-"; // 获取月份。
-    s += day; // 获取日。
-    return (s); // 返回日期。
-};
 
 function getAll(begin, end) {
     var ab = begin;
